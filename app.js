1f2101f974aa0a9f974a58c26f6e9b71f63f69c9
Maniulo <m.mezhenin@gmail.com>
2013-01-06 01:43:45 +0600

    #new Simple objects and mouse handler

--- app.js
+++ app.js
@@ -1,11 +1,89 @@
+// Globalizing LibCanvas variables
 LibCanvas.extract();
 
-atom.dom(function() {
-   var canvas  = atom.dom('canvas').first;
-   var context = canvas.getContext('2d-libcanvas');
+// onDomReady - run our app
+atom.dom(function () {
+	new Circles.Controller();
+});
 
-   context
-      .fillAll( '#efebe7' )
-      .fill( new Rectangle( 75, 75, 30, 30 ), 'green' )
-      .fill( new Circle   ( 50, 50, 20 )    , '#c00'  );
+
+/** @class Pong.Controller */
+atom.declare( 'Circles.Controller', {
+
+	initialize: function () {
+		this.size  = new Size(500, 500);
+		this.app   = new App({ size: this.size });
+		this.layer = this.app.createLayer({ invoke: true });
+			
+		mouse 	     = new Mouse(this.app.container.bounds);
+		mouseHandler = new App.MouseHandler({ app: this.app, mouse: mouse });
+		
+		this.field = new Circles.Field( this.layer, {
+				controller: this,
+				size: this.size
+		});
+		
+		mouseHandler.subscribe( this.field );
+		
+		this.field.events.add( 'click', function(e) {
+			console.log( 'element поймал клик мыши', e );
+			c = new Circles.Circle( this.layer, {
+				controller: this,
+				size: this.size,
+				x: e.x,
+				y: e.y
+			});
+		})
+	}
+});
+
+atom.declare('Circles.Circle', App.Element, {
+
+	configure: function()	{
+		x = this.settings.get('x');
+		y = this.settings.get('y');
+		
+		this.shape = new Circle(
+			x,
+			y,
+			100
+		);
+	},
+	
+	speed: 10,
+	
+	
+	onUpdate: function (time) {
+		// вращаемся со скоростью 90 градусов в секунду
+		this.shape.radius -= time / this.speed; //( (90).degree() * time / 1000 );
+		
+		if (this.shape.radius <= 0)
+		{
+			this.destroy();
+		}
+		else
+		{
+			this.redraw();
+		}
+    },
+	
+	renderTo: function (ctx, resources) {
+        ctx.fill( this.shape, 'red' );
+    }
+});
+
+atom.declare('Circles.Field', App.Element, {
+
+	configure: function()	{
+		this.shape = new Rectangle(
+			0,
+			0,
+			500,
+			500
+		);
+	},
+	
+	renderTo: function (ctx, resources) {
+        ctx.fill( this.shape, '#dedede' );
+    }
 });
